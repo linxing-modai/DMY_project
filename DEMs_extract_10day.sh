aba57be3295c18ae1fb08110eab3145fcf68563b
linxing-modai <1067889949@qq.com>
2024-12-29 11:02:57 +0800

    delete

--- DEMs_extract_10day.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cat 10day_XXXYD_deseq2_res.csv |sed 's/,/\t/g' |awk '$3 >=1' |awk '$7 <= 0.05' |sed 's/"//g' |awk '{print $1}' >10d_XXXYD_orth_XYD_high_degs_list1.csv
-cat 10day_XXXYD_deseq2_res.csv |sed 's/,/\t/g' |awk '$3 <=-1' |awk '$7 <= 0.05' |grep -v padj|sed 's/"//g' |awk '{print $1}' >10d_XXXYD_orth_XX_high_degs_list1.csv
-cat 10day_XXXY_deseq2_res.csv |sed 's/,/\t/g' |awk '$3 >=1' |awk '$7 <= 0.05' |sed 's/"//g' |awk '{print $1}' >10d_XXXY_orth_XY_high_degs_list1.csv
-cat 10day_XXXY_deseq2_res.csv |sed 's/,/\t/g' |awk '$3 <=-1' |awk '$7 <= 0.05' |grep -v padj|sed 's/"//g' |awk '{print $1}' >10d_XXXY_orth_XX_high_degs_list1.csv
-cat 10day_XYDXY_deseq2_res.csv |sed 's/,/\t/g' |awk '$3 >=1' |awk '$7 <= 0.05' |sed 's/"//g' |awk '{print $1}' >10d_XYDXY_orth_XYD_high_degs_list1.csv
-cat 10day_XYDXY_deseq2_res.csv |sed 's/,/\t/g' |awk '$3 <=-1' |awk '$7 <= 0.05' |grep -v padj|sed 's/"//g' |awk '{print $1}' >10d_XYDXY_orth_XY_high_degs_list1.csv
